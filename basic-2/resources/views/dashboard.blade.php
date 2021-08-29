805a831676123b404e75184457f44f47f8945c0f
NelsonWang <wangnelson2@gmail.com>
2021-08-29 15:16:58 -0400

    dasgboard | date

--- basic-2/resources/views/dashboard.blade.php
+++ basic-2/resources/views/dashboard.blade.php
@@ -14,10 +14,9 @@
                         <table class="table">
                             <thead>
                                 <tr>
-                                    <th scope="col">#</th>
-                                    <th scope="col">First</th>
-                                    <th scope="col">Last</th>
-                                    <th scope="col">Handle</th>
+                                    <th scope="col">Name</th>
+                                    <th scope="col">Mail</th>
+                                    <th scope="col">Created at</th>
                                 </tr>
                             </thead>
                             <tbody>
@@ -25,10 +24,9 @@
                                 @foreach($users as $user)
 
                                 <tr>
-                                    <th scope="row">1</th>
                                     <td>{{$user->name}}</td>
-                                    <td>Otto</td>
-                                    <td>@mdo</td>
+                                    <td>{{$user->email}}</td>
+                                    <td>{{$user->created_at}}</td>
                                 </tr>
 
                                 @endforeach
