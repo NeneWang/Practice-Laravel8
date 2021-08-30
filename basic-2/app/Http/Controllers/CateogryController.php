d55bf1a49ce91a733f500b634dc3b6bbb118e82b
NelsonWang <wangnelson2@gmail.com>
2021-08-29 23:18:11 -0400

    models | categories fixed

--- basic-2/app/Http/Controllers/CateogryController.php
+++ basic-2/app/Http/Controllers/CateogryController.php
@@ -1,9 +1,11 @@
 <?php
 
 namespace App\Http\Controllers;
+
 use App\Models\Category;
 
 use Illuminate\Http\Request;
+// use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Carbon;
 
@@ -18,17 +20,22 @@ class CateogryController extends Controller
     {
         $validatedData = $request->validate([
             'category_name' => 'required|unique:categories|max:255',
-            'body' => 'required',
         ], [
             'category_name.required' => 'Please Input Category Name',
-            'category_name.max' => 'Category Less than 255 chars'
+            'category_name.max' => 'Category has to be less than 255 chars'
         ]);
 
         Category::insert([
-            'category_name'=> $request->category_name,
+            'category_name' => $request->category_name,
             'user_id' => Auth::user()->id,
-            'created_at'=>Carbon::now(),
+            'created_at' => Carbon::now()
         ]);
+        // $category = new Category;
+        // $category->category_name = $request->category_name;
+        // $category->user_id = Auth::user()->id;
+        // $category->save();
 
+
+        return Redirect()->back()->with('Success', 'Category Inserted Successfull');
     }
 }
